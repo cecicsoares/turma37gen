5336b304d3597b5d92d208846a302622a6e64574
unknown <cecilia.soares13@gmail.com>
2021-11-19 09:26:04 -0300

    SECURITY BLOG PESSOAL

---
+++ reintegra_projetointegrador/reintegra/src/main/java/com/reintegra/ReintegraApplication.java
@@ -0,0 +1,13 @@
+package com.reintegra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReintegraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReintegraApplication.class, args);
+	}
+
+}
---
+++ reintegra_projetointegrador/reintegra/src/test/java/com/reintegra/ReintegraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reintegra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReintegraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
